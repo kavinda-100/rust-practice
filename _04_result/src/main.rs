83a792455634708e1a46d4e6c946722c28545bef
kavinda-100 <kavindarathnayake100@gmail.com>
2025-11-19 11:40:13 +0530

    Initialize _04_result project with Cargo.toml, Cargo.lock, and main.rs

---
+++ _04_result/src/main.rs
@@ -0,0 +1,11 @@
+/**
+ * @Author- Kavinda Rathnayake
+ * @Date- 2025-11-19
+ * @title- 04_result
+ * @Description- This sub-directory is for practicing Rust Result.
+ *               It contains a Cargo project with the necessary files.
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
