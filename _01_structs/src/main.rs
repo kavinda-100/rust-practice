7bb298b2124d758e04a6861646ad363b67bafa40
kavinda-100 <kavindarathnayake100@gmail.com>
2025-11-19 10:52:03 +0530

    Update .gitignore to include additional target directories and enhance documentation in main.rs

--- _01_structs/src/main.rs
+++ _01_structs/src/main.rs
@@ -1,3 +1,11 @@
+/**
+ * @Author- Kavinda Rathnayake
+ * @Date- 2025-11-19
+ * @title- 01_structs
+ * @Description- This sub-directory is for practicing Rust structs.
+ *               It contains a Cargo project with the necessary files.
+ */
+
 fn main() {
     println!("Hello, world!");
 }
