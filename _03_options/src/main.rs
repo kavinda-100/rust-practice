17dbb206b259032cc224d6fdae0cf123e81fb1da
kavinda-100 <kavindarathnayake100@gmail.com>
2025-11-19 11:37:57 +0530

    Implement division and data fetching functions with error handling

--- _03_options/src/main.rs
+++ _03_options/src/main.rs
@@ -1,3 +1,54 @@
+/**
+ * @Author- Kavinda Rathnayake
+ * @Date- 2025-11-19
+ * @title- 03_options
+ * @Description- This sub-directory is for practicing Rust options.
+ *               It contains a Cargo project with the necessary files.
+ */
+
+ // Example code for practicing Rust options
+ fn divide(numerator: f64, denominator: f64) -> Option<f64> {
+    if denominator == 0.0 {
+        None
+    } else {
+        Some(numerator / denominator)
+    }
+}
+
+// function that simulates fetch request that return Option
+fn fetch_data(id: u32) -> Option<String> {
+    let database = vec![
+        (1, "Data for ID 1".to_string()),
+        (2, "Data for ID 2".to_string()),
+        (3, "Data for ID 3".to_string()),
+    ];
+    // Simulate fetching data
+    for (db_id, data) in database {
+        if db_id == id {
+            return Some(data);
+        }
+    }
+    None
+}
+
+ // Main function
+
 fn main() {
-    println!("Hello, world!");
+    
+    println!("Division result: \n");
+    let result = divide(10.0, 2.0);
+    match result {
+        Some(value) => println!("Result: {}", value),
+        None => println!("Error: Division by zero"),
+    }
+
+    println!();
+
+    println!("Fetching data: \n");
+    let data_id = 20;
+    let data = fetch_data(data_id);
+    match data {
+        Some(content) => println!("Fetched data: {}", content),
+        None => println!("No data found for ID {}", data_id),
+    }
 }
